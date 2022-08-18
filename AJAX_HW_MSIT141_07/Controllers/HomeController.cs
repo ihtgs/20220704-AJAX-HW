bbd2660d705d4f7d0f5e460c37ce4b2e0139ab19
張正昱 <ihtgschen@gmail.com>
2022-08-18 21:46:24 +0800

    加入專案檔案。

---
+++ AJAX_HW_MSIT141_07/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using AJAX_HW_MSIT141_07.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJAX_HW_MSIT141_07.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DemoContext _context;
+
+        public HomeController(ILogger<HomeController> logger, DemoContext conetxt)
+        {
+
+            _logger = logger;
+            _context = conetxt;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult FirstAjax()
+        {
+            return View();
+        }
+
+        public IActionResult AjaxPost()
+        {
+            return View();
+        }
+        public IActionResult AjaxPostFormData()
+        {
+            return View();
+        }
+        public IActionResult Address()
+        {
+            return View();
+        }
+
+        public IActionResult promise()
+        {
+            return View();
+        }
+
+        public IActionResult Fetch()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        public IActionResult jQuery()
+        {
+            return View();
+        }
+
+        public IActionResult ShipperCors()
+        {
+            return View();
+        }
+
+        public IActionResult ShipperCorsEmpty()
+        {
+            return View();
+        }
+        public IActionResult Partial()
+        {
+            ViewBag.data = "Hello Partial!!";
+            return PartialView(_context.Members);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# AJAX_HW_MSIT141_07
+### AJAX作業(07_張正昱)
+
+- 0701：按鈕讀取JSON字串顯示表格(/Home/Index) 
+- 0704：blur()確認帳號是否已存在(/Home/Register、/Api/NameCheck)
+- 0705：async+await的地址選項(/Home/Address、/Api/City、/Api/District、/Api/Road)
+- 0705：手工寫自動提詞功能(/Home/Address、/Api/GetKeyWords)
